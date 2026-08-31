395412338a579eb7ad154ce8cee0c80e7814a8f5
Ellandq <waselland@gmail.com>
2026-08-31 22:13:51 +0200

    Moved server configuration to separate class

--- src/Communicator.Server/Program.cs
+++ src/Communicator.Server/Program.cs
@@ -1,36 +1,13 @@
-using Communicator.Server.Realtime;
-using Communicator.Server.Realtime.Connections;
-using Communicator.Server.Realtime.Messaging;
+using Communicator.Server;
+using Communicator.Server.Configuration;
 
 var builder = WebApplication.CreateBuilder(args);
 
-#region DEPENDENCY INJECTION REGISTRATION
-
-builder.Services.AddSingleton<ConnectionManager>();
-builder.Services.AddSingleton<WebSocketMessageReader>();
-
-builder.Services.AddTransient<WebSocketHandler>();
-builder.Services.AddTransient<WebSocketEndpoint>();
-
-#endregion
+builder.Services.AddCommunicatorServer();
 
 var app = builder.Build();
 
-app.UseWebSockets();
-
-#region ENDPOINTS
-
-app.MapGet("/", () => "Communicator server is running.");
-
-app.Map("/ws", async context =>
-{
-    var endpoint = context.RequestServices
-        .GetRequiredService<WebSocketEndpoint>();
-
-    await endpoint.HandleAsync(context);
-});
-
-#endregion
+app.UseCommunicatorServer();
 
 app.Run();
 
