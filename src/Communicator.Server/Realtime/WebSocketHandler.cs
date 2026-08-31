c2ab0ec25af5088d6e4ba4ea8b7dc23278f94de3
Ellandq <waselland@gmail.com>
2026-08-31 17:41:33 +0200

    Updated handler logic and implementation

--- src/Communicator.Server/Realtime/WebSocketHandler.cs
+++ src/Communicator.Server/Realtime/WebSocketHandler.cs
@@ -1,100 +1,59 @@
 ﻿using System.Net.WebSockets;
-using System.Text;
+using Communicator.Server.Realtime.Connections;
+using Communicator.Server.Realtime.Messaging;
 
 namespace Communicator.Server.Realtime;
 
-public static class WebSocketHandler
+public sealed class WebSocketHandler(
+    ConnectionManager connectionManager,
+    WebSocketMessageReader webSocketMessageReader)
 {
-    public static async Task HandleAsync(HttpContext context)
+    public async Task HandleAsync(
+        WebSocket webSocket,
+        CancellationToken cancellationToken)
     {
-        if (!context.WebSockets.IsWebSocketRequest)
+        var connection = new WebSocketClientConnection(
+            webSocket, 
+            webSocketMessageReader);
+
+        connectionManager.AddConnection(connection);
+
+        try
         {
-            context.Response.StatusCode = StatusCodes.Status400BadRequest;
-            return;
+            await ReceiveMessagesAsync(
+                connection,
+                cancellationToken);
         }
-
-        using var webSocket =
-            await context.WebSockets.AcceptWebSocketAsync();
-        
-        Console.WriteLine("WebSocket client connected.");
-
-        await ReceiveMessagesAsync(webSocket);
-    }
-
-    private static async Task ReceiveMessagesAsync(WebSocket webSocket)
-    {
-        var buffer = new byte[4096];
-
-        while (webSocket.State == WebSocketState.Open)
+        finally
         {
-            WebSocketReceiveResult? result = null;
+            connectionManager.RemoveConnection(connection.Id);
             
-            using var ms = new MemoryStream();
-            do
-            {
-                result = await webSocket.ReceiveAsync(
-                    buffer,
-                    CancellationToken.None);
-
-                ms.Write(
-                    buffer,
-                    0,
-                    result.Count);
-                
-            } while (!result.EndOfMessage);
-
-            ms.Seek(0, SeekOrigin.Begin);
-                        
-            switch (result.MessageType)
-            {
-                case WebSocketMessageType.Close:
-                    await CloseConnectionAsync(webSocket, "User requested socket closure.");
-                    return;
-                
-                case WebSocketMessageType.Text:
-                {
-                    using var reader = new StreamReader(ms, Encoding.UTF8);
-                    var message = await reader.ReadToEndAsync();
-                        
-                    Console.WriteLine($"Received: {message}");
-                
-                    await SendMessageAsync(
-                        webSocket, 
-                        $"Server received: {message}");
-                    break;
-                }
-                
-                case WebSocketMessageType.Binary:
-                    break;
-                
-                default:
-                    throw new ArgumentOutOfRangeException();
-            }
+            connection.Dispose();
         }
     }
 
-    private static async Task SendMessageAsync(
-        WebSocket webSocket,
-        string message)
+    private async Task ReceiveMessagesAsync(
+        IClientConnection connection,
+        CancellationToken cancellationToken)
     {
-        var data = Encoding.UTF8.GetBytes(message);
+        while (connection.IsOpen)
+        {
+            var result = await connection.ReceiveAsync(
+                cancellationToken);
 
-        await webSocket.SendAsync(
-            data,
-            WebSocketMessageType.Text,
-            true,
-            CancellationToken.None);
+            if (result is null)
+            {
+                await connection.CloseAsync(
+                    WebSocketCloseStatus.NormalClosure,
+                    "Connection closed by client.",
+                    cancellationToken);
+                
+                return;
+            }
+            
+            // TODO
+        }
     }
 
-    private static async Task CloseConnectionAsync(
-        WebSocket webSocket,
-        string message)
-    {
-        Console.WriteLine("WebSocket client disconnected.");
 
-        await webSocket.CloseAsync(
-            WebSocketCloseStatus.NormalClosure,
-            message,
-            CancellationToken.None);
-    }
 }
