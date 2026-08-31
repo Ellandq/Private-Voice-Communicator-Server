6a143b74c7c1c48f855b5868921a3a02b7a8fb3f
Ellandq <waselland@gmail.com>
2026-08-31 22:15:40 +0200

    Message handler interface and implementation

---
+++ src/Communicator.Server/Realtime/Protocol/Handlers/RealtimeMessageHandler.cs
@@ -0,0 +1,39 @@
+﻿using System.Text.Json;
+
+namespace Communicator.Server.Realtime.Protocol.Handlers;
+
+public abstract class RealtimeMessageHandler<TPayload>
+    : IRealtimeMessageHandler<TPayload>
+{
+    public abstract string MessageType { get; }
+    
+    public async Task HandleAsync(
+        RealtimeMessage message, 
+        CancellationToken cancellationToken = default)
+    {
+        if (message.Payload is null)
+        {
+            throw new JsonException(
+                $"Message '{message.Type}' requires a payload");
+        }
+
+        var payload = message.Payload.Value
+            .Deserialize<TPayload>();
+
+        if (payload is null)
+        {
+            throw new JsonException(
+                $"Could not deserialize payload for '{message.Type}'.");
+        }
+
+        await HandleAsync(
+            payload,
+            message.RequestId,
+            cancellationToken);
+    }
+
+    public abstract Task HandleAsync(
+        TPayload payload,
+        Guid? requestId,
+        CancellationToken cancellationToken = default);
+}
---
+++ src/Communicator.Server/Realtime/Protocol/MessageTypes.cs
@@ -0,0 +1,7 @@
+﻿namespace Communicator.Server.Realtime.Protocol;
+
+public static class MessageTypes
+{
+    public const string MessageSend = "message.send";
+    public const string MessageCreated = "message.created";
+}
