62c8b0eb826fc3ee148ee15226ab28925c95b908
Ellandq <waselland@gmail.com>
2026-08-31 17:39:44 +0200

    Separated message reader

---
+++ src/Communicator.Server/Realtime/Messaging/WebSocketMessageReader.cs
@@ -0,0 +1,40 @@
+﻿using System.Net.WebSockets;
+
+namespace Communicator.Server.Realtime.Messaging;
+
+public sealed class WebSocketMessageReader
+{
+    private const int BufferSize = 4096;
+
+    public async Task<MessageReadResult?> ReadAsync(
+        WebSocket webSocket,
+        CancellationToken cancellationToken = default)
+    {
+        var buffer = new byte[BufferSize];
+
+        using var messageStream = new MemoryStream();
+
+        WebSocketReceiveResult result;
+
+        do
+        {
+            result = await webSocket.ReceiveAsync(
+                buffer,
+                cancellationToken);
+
+            if (result.MessageType == WebSocketMessageType.Close)
+            {
+                return null;
+            }
+
+            messageStream.Write(
+                buffer,
+                0,
+                result.Count);
+        } while (!result.EndOfMessage);
+
+        return new MessageReadResult(
+            result.MessageType,
+            messageStream.ToArray());
+    }
+}
