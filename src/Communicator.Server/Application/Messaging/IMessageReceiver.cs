3dea9c4e9fde647c8a7556ed4a01a9348fc2b481
Ellandq <waselland@gmail.com>
2026-08-31 22:18:00 +0200

    Message receiver and implementation

---
+++ src/Communicator.Server/Application/Messaging/IMessageReceiver.cs
@@ -0,0 +1,11 @@
+﻿using Communicator.Server.Realtime.Protocol.Requests;
+
+namespace Communicator.Server.Application.Messaging;
+
+public interface IMessageReceiver
+{
+    Task ReceiveAsync(
+        SendMessageRequest request,
+        Guid? requestId,
+        CancellationToken cancellationToken = default);
+}
---
+++ src/Communicator.Server/Application/Messaging/MessageReceiver.cs
@@ -0,0 +1,20 @@
+﻿using Communicator.Server.Realtime.Protocol.Requests;
+
+namespace Communicator.Server.Application.Messaging;
+
+public sealed class MessageReceiver
+    : IMessageReceiver
+{
+    public Task ReceiveAsync(
+        SendMessageRequest request,
+        Guid? requestId,
+        CancellationToken cancellationToken = default)
+    {
+        Console.WriteLine(
+            $"Send message request: " +
+            $"Conversation={request.ConversationId}, " +
+            $"Content={request.Content}");
+
+        return Task.CompletedTask;
+    }
+}
