08bd6169d96bcbd399ec8b720e9ccbb79bfeafa6
Ellandq <waselland@gmail.com>
2026-08-31 14:36:30 +0200

    Added basic WebSocket handling

---
+++ src/Communicator.Server/Realtime/WebSocketHandler.cs
@@ -0,0 +1,101 @@
+﻿using System.Net.WebSockets;
+using System.Text;
+
+namespace Communicator.Server.Realtime;
+
+public static class WebSocketHandler
+{
+    public static async Task HandleAsync(HttpContext context)
+    {
+        if (!context.WebSockets.IsWebSocketRequest)
+        {
+            context.Response.StatusCode = StatusCodes.Status400BadRequest;
+            return;
+        }
+
+        using var webSocket =
+            await context.WebSockets.AcceptWebSocketAsync();
+        
+        Console.WriteLine("WebSocket client connected.");
+
+        await ReceiveMessagesAsync(webSocket);
+    }
+
+    private static async Task ReceiveMessagesAsync(WebSocket webSocket)
+    {
+        var buffer = new ArraySegment<byte>(new byte[4096]);
+
+        while (webSocket.State == WebSocketState.Open)
+        {
+            WebSocketReceiveResult? result = null;
+            
+            using var ms = new MemoryStream();
+            do
+            {
+                result = await webSocket.ReceiveAsync(
+                    buffer,
+                    CancellationToken.None
+                );
+                
+                ms.Write(
+                    buffer.Array!,
+                    buffer.Offset,
+                    result.Count);
+                
+            } while (!result.EndOfMessage);
+
+            ms.Seek(0, SeekOrigin.Begin);
+                        
+            switch (result.MessageType)
+            {
+                case WebSocketMessageType.Close:
+                    await CloseConnectionAsync(webSocket, "User requested socket closure.");
+                    return;
+                
+                case WebSocketMessageType.Text:
+                {
+                    using var reader = new StreamReader(ms, Encoding.UTF8);
+                    var message = await reader.ReadToEndAsync();
+                        
+                    Console.WriteLine($"Received: {message}");
+                
+                    await SendMessageAsync(
+                        webSocket, 
+                        $"Server received: {message}");
+                    break;
+                }
+                
+                case WebSocketMessageType.Binary:
+                    break;
+                
+                default:
+                    throw new ArgumentOutOfRangeException();
+            }
+        }
+    }
+
+    private static async Task SendMessageAsync(
+        WebSocket webSocket,
+        string message)
+    {
+        var data = Encoding.UTF8.GetBytes(message);
+
+        await webSocket.SendAsync(
+            new ArraySegment<byte>(data),
+            WebSocketMessageType.Text,
+            true,
+            CancellationToken.None);
+    }
+
+    private static async Task CloseConnectionAsync(
+        WebSocket webSocket,
+        string message)
+    {
+        Console.WriteLine("WebSocket client disconnected.");
+
+        await webSocket.CloseAsync(
+            WebSocketCloseStatus.NormalClosure,
+            message,
+            CancellationToken.None);
+    }
+}
