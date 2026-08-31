28c752ffc5c98457954c43e31aec81567f5e180c
Ellandq <waselland@gmail.com>
2026-08-31 17:41:46 +0200

    Adjusted main class structure

--- src/Communicator.Server/Program.cs
+++ src/Communicator.Server/Program.cs
@@ -1,8 +1,18 @@
-using System.Net.WebSockets;
-using System.Text;
 using Communicator.Server.Realtime;
+using Communicator.Server.Realtime.Connections;
+using Communicator.Server.Realtime.Messaging;
 
 var builder = WebApplication.CreateBuilder(args);
+
+#region DEPENDENCY INJECTION REGISTRATION
+
+builder.Services.AddSingleton<ConnectionManager>();
+builder.Services.AddSingleton<WebSocketMessageReader>();
+
+builder.Services.AddTransient<WebSocketHandler>();
+builder.Services.AddTransient<WebSocketEndpoint>();
+
+#endregion
 
 var app = builder.Build();
 
@@ -12,7 +22,13 @@ app.UseWebSockets();
 
 app.MapGet("/", () => "Communicator server is running.");
 
-app.Map("/ws", WebSocketHandler.HandleAsync);
+app.Map("/ws", async context =>
+{
+    var endpoint = context.RequestServices
+        .GetRequiredService<WebSocketEndpoint>();
+
+    await endpoint.HandleAsync(context);
+});
 
 #endregion
 
